39fa7f6e2f1a8d17a72eb2852c13ce0a9c272a9b
Зиновьев Кирилл <kirillzinovev23@gmail.com>
2024-03-12 11:54:25 +0300

    feef

--- middleware/passport_go.js
+++ middleware/passport_go.js
@@ -27,6 +27,7 @@ function passportFunctionGoogle(passport) {
       function (request, accessToken, refreshToken, profile, done) {
         // asynchronous verification, for effect...
         process.nextTick(function () {
+          //qwve
           // To keep the example simple, the user's Google profile is returned
           // to represent the logged-in user.  In a typical application, you would
           // want to associate the Google account with a user record in your
